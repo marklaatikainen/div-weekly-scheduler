8f3acd8d9e25a1ae2a153a22a702073aa2edf1ae
Mark Laatikainen <marklaatikainen@users.noreply.github.com>
2017-10-31 14:40:18 +0200

    Add files via upload

---
+++ calendarGrid.js
@@ -0,0 +1,116 @@
+function getJSON() {
+	var jqxhr = $.getJSON( "data.json", function() {
+	})
+  .success(function(response) {
+	createEvents(response);
+  })
+  .fail(function() {
+    console.log( "error" );
+	return;
+  })
+}
+
+function createGrid() {
+	var days = ["","Maanantai","Tiistai","Keskiviikko","Torstai","Perjantai"];
+	var times = ["8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00"];
+
+	var doc = document.getElementById("calendar");
+
+	// create headers
+	var headerRow = document.createElement("div");
+		headerRow.className = "headerRow";
+		
+	for (var d = 0; d < days.length; d++) {
+		var header = document.createElement("div");
+			header.className = "day";
+			header.innerText = days[d];
+			headerRow.appendChild(header);
+	}
+	doc.appendChild(headerRow);
+	
+	// create columns
+	for (var j = 0; j < 6; j++) {
+		var col = document.createElement("div");
+			col.className = "column";
+		// create cells
+		for(var i = 0; i < 13; i++) {
+			var cell = document.createElement("div");
+				cell.className = "cell";
+			//	cell.innerText = j;
+			
+			if (j  == 0) {
+				cell.innerHTML = times[i];
+				cell.className = "time";
+			}
+			col.appendChild(cell);
+		}
+		doc.appendChild(col);
+	}
+	
+	getJSON();
+}
+
+function createEvents(data) {
+	var doc = document.getElementById("calendar");
+	//Loop data
+	for(var d = 0; d < data.length; d++) {
+	var ev = document.createElement("div");
+		ev.className = "event";
+    	var stmz = [];
+    	var etmz = [];
+		// Loop schedule
+		for (var s = 0; s < data[d].Time.length; s++) {
+			var cont = data[d].Id + "<br>" + data[d].Name + "<br>" + data[d].Time[s].substring(5, 10) + " - " + data[d].Time[s].substring(11, 16);
+			console.log(cont);
+			ev.innerHTML = cont;
+			ev.style.top =  getPosition(data[d].Time[s].substring(5, 10)) + "px";
+			ev.style.left = getDay(data[d].Time[s].substring(1,4)) + "px";
+            ev.style.height = data[d].Time[s].substring(11, 16) + "%";
+		}
+	}
+	doc.appendChild(ev);
+}
+
+function getPosition() {
+	
+}
+
+function getDay(day) {
+	if (day == "Mon") {
+		return 340;
+	} else if (day == "Tue") {
+		return 440;
+	} else if (day == "Wed") {
+		return 540;
+	} else if (day == "Thu") {
+		return 640;
+	} else if (day == "Fri") {
+		return 740;
+	} else {
+		return;
+	}
+}
+
+function getDuration(startTime, endTime) {
+    // start time
+    var sDate = new Date();
+    var s = startTime.split(':');
+    sDate.setHours(s[0]);
+    sDate.setMinutes(s[1]);
+    // end time
+    var eDate = new Date();
+    var e = endTime.split(':');
+    eDate.setHours(e[0]);
+    eDate.setMinutes(e[1]);
+
+    var elapsed = eDate - sDate; // time in milliseconds
+    var difference = new Date(elapsed);
+    var diff_hours = difference.getHours();
+    var diff_mins = difference.getMinutes();
+
+    var duration = diff_hours;
+    if (e[1] == "45" || e[1] == "40") {
+        duration -= 1;
+    }
+    return duration;
+}
